c1d8275a1939452f19819b9de01e28a8ea2b44d1
Chmichev Boris <boris362@yandex.ru>
2017-04-27 19:49:35 +0300

    To ES5

--- nthline.js
+++ nthline.js
@@ -1,22 +1,23 @@
-const readline = require('readline')
+var readline = require('readline')
   , fs = require('fs')
   , bb = require('bluebird')
 
-const outOfRangeError = (filepath, n, cursor) =>
-  new RangeError(
+var outOfRangeError = function(filepath, n, cursor) {
+  return new RangeError(
     `Requested line ${n} but '${filepath}' has only ${cursor} lines`
   )
+}
 
-module.exports = (n, filepath) =>
-  new bb((resolve, reject) => {
+module.exports = function (n, filepath) {
+  return new bb(function(resolve, reject) {
     if (n < 0 || n % 1 !== 0)
       return reject(new RangeError(`Invalid line number`))
 
-    let cursor = 0
-    const input = fs.createReadStream(filepath)
+    var cursor = 0
+      , input = fs.createReadStream(filepath)
       , rl = readline.createInterface({ input })
 
-    rl.on('line', line => {
+    rl.on('line', function(line) {
       if (cursor++ === n) {
         rl.close()
         resolve(line)
@@ -24,7 +25,8 @@ module.exports = (n, filepath) =>
     })
 
     rl.on('error', reject)
-    input.on('end', () =>
+    input.on('end', function() {
       reject(outOfRangeError(filepath, n, cursor))
-    )
+    })
   })
+}
